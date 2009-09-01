e396396aa0e7775f36741096359651ee397367f3
Osmo Salomaa <otsaloma@cc.hut.fi>
2009-09-02 01:04:17 +0300

    Add new module with monkey_patch decorator.

---
+++ nfoview/deco.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2009 Osmo Salomaa
+#
+# This file is part of NFO Viewer.
+#
+# NFO Viewer is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# NFO Viewer is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# NFO Viewer. If not, see <http://www.gnu.org/licenses/>.
+
+"""Miscellaneous decorators for functions and methods."""
+
+import copy
+import functools
+
+
+def monkey_patch(obj, name):
+    """Save obj's attribute and restore it after function returns.
+
+    Attribute in question must be able to handle copy.deepcopy.
+    """
+    def outer_wrapper(function):
+        @functools.wraps(function)
+        def inner_wrapper(*args, **kwargs):
+            attr = getattr(obj, name)
+            setattr(obj, name, copy.deepcopy(attr))
+            try: return function(*args, **kwargs)
+            finally:
+                setattr(obj, name, attr)
+                assert getattr(obj, name) == attr
+                assert getattr(obj, name) is attr
+        return inner_wrapper
+    return outer_wrapper
