917792a361acad07c88b4b48d1a343a1ba9c1a9d
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-05-21 18:38:13 +0300

    Update setup.py.

--- setup.py
+++ setup.py
@@ -17,40 +17,49 @@ process: (1) writing the nfoview.paths module and (2) handling translations.
     specifically, executables 'msgfmt' and 'intltool-merge' in $PATH.
 """
 
-import distutils
 import distutils.command.clean
 import distutils.command.install
 import distutils.command.install_data
 import distutils.command.install_lib
 import distutils.command.sdist
 import glob
-import itertools
 import os
 import re
 import shutil
 import subprocess
+import sys
 import tarfile
 import tempfile
 
-log = distutils.log
-
 os.chdir(os.path.dirname(__file__) or ".")
 
+clean = distutils.command.clean.clean
+command = distutils.cmd.Command
+install = distutils.command.install.install
+install_data = distutils.command.install_data.install_data
+install_lib = distutils.command.install_lib.install_lib
+log = distutils.log
+sdist = distutils.command.sdist.sdist
 
-def get_version_number():
+
+def get_version():
     """Return version number from nfoview/__init__.py."""
-    # Cannot import this, because importing __version__ from nfoview causes
-    # a segfault if building or compiling outside X (with no $DISPLAY).
     text = open(os.path.join("nfoview", "__init__.py"), "r").read()
     return re.search(r"__version__ *= *['\"](.*?)['\"]", text).group(1)
 
 def run_command_or_exit(command):
     """Run command in shell and raise SystemExit if it fails."""
-    if os.system(command) != 0:
-        raise SystemExit("Error: Command %s failed." % repr(command))
+    if subprocess.call(command) != 0:
+        log.error("command %s failed" % repr(command))
+        raise SystemExit(1)
+
+def run_command_or_warn(command):
+    """Run command in shell and raise SystemExit if it fails."""
+    if subprocess.call(command) != 0:
+        log.warn("command %s failed" % repr(command))
 
 
-class Clean(distutils.command.clean.clean):
+class Clean(clean):
 
     """Command to remove files and directories created."""
 
@@ -71,21 +80,19 @@ class Clean(distutils.command.clean.clean):
 
     def run(self):
         """Remove files and directories listed in self.__glob_targets."""
-        distutils.command.clean.clean.run(self)
-        targets = map(glob.glob, self.__glob_targets)
-        iterator = itertools.chain(*targets)
-        for target in filter(os.path.isdir, iterator):
-            log.info("removing '%s'" % target)
-            if not self.dry_run:
-                shutil.rmtree(target)
-        iterator = itertools.chain(*targets)
-        for target in filter(os.path.isfile, iterator):
-            log.info("removing '%s'" % target)
-            if not self.dry_run:
-                os.remove(target)
+        clean.run(self)
+        for targets in map(glob.glob, self.__glob_targets):
+            for target in filter(os.path.isdir, targets):
+                log.info("removing '%s'" % target)
+                if not self.dry_run:
+                    shutil.rmtree(target)
+            for target in filter(os.path.isfile, targets):
+                log.info("removing '%s'" % target)
+                if not self.dry_run:
+                    os.remove(target)
 
 
-class Documentation(distutils.cmd.Command):
+class Documentation(command):
 
     """Command to build documentation from source code."""
 
@@ -108,17 +115,19 @@ class Documentation(distutils.cmd.Command):
         os.chdir(os.path.join("doc", "sphinx"))
         if not self.dry_run:
             run_command_or_exit("make clean")
-            run_command_or_exit("python autogen.py nfoview")
+            run_command_or_exit("python%d.%d autogen.py nfoview"
+                                % sys.version_info[:2])
+
             run_command_or_exit("make %s" % self.format)
 
 
-class Install(distutils.command.install.install):
+class Install(install):
 
     """Command to install everything."""
 
     def run(self):
         """Install everything and update the desktop file database."""
-        distutils.command.install.install.run(self)
+        install.run(self)
         get_command_obj = self.distribution.get_command_obj
         root = get_command_obj("install").root
         data_dir = get_command_obj("install_data").install_dir
@@ -126,11 +135,10 @@ class Install(distutils.command.install.install):
         if (root is not None) or (data_dir is None): return
         directory = os.path.join(data_dir, "share", "applications")
         log.info("updating desktop database in '%s'" % directory)
-        try: subprocess.call(("update-desktop-database", directory))
-        except OSError: log.info("...failed")
+        run_command_or_warn(("update-desktop-database", directory))
 
 
-class InstallData(distutils.command.install_data.install_data):
+class InstallData(install_data):
 
     """Command to install data files."""
 
@@ -160,19 +168,20 @@ class InstallData(distutils.command.install_data.install_data):
         for po_file in glob.glob("po/*.po"):
             self.data_files.append(self.__get_mo_file(po_file))
         self.data_files.append(self.__get_desktop_file())
-        distutils.command.install_data.install_data.run(self)
+        install_data.run(self)
 
 
-class InstallLib(distutils.command.install_lib.install_lib):
+class InstallLib(install_lib):
 
     """Command to install library files."""
 
     def install(self):
         """Install library files after writing changes."""
-        # Allow --root to be used as a destination directory.
-        root = self.distribution.get_command_obj("install").root
-        prefix = self.distribution.get_command_obj("install").install_data
-        if not None in (root, prefix):
+        get_command_obj = self.distribution.get_command_obj
+        root = get_command_obj("install").root
+        prefix = get_command_obj("install").install_data
+        # Allow --root to be used like $DESTDIR.
+        if root is not None:
             prefix = os.path.abspath(prefix)
             prefix = prefix.replace(os.path.abspath(root), "")
         data_dir = os.path.join(prefix, "share", "nfoview")
@@ -189,32 +198,32 @@ class InstallLib(distutils.command.install_lib.install_lib):
         text = text.replace(patt, repl)
         assert text.count(repr(locale_dir)) > 0
         open(path, "w").write(text)
-        return distutils.command.install_lib.install_lib.install(self)
+        return install_lib.install(self)
 
 
-class SDistGna(distutils.command.sdist.sdist):
+class SDistGna(sdist):
 
     """Command to create a source distribution for gna.org."""
 
-    __version = get_version_number()
     description = "create source distribution for gna.org"
 
     def finalize_options(self):
         """Set the distribution directory to 'dist/X.Y'."""
-        # pylint: disable-msg=W0201
-        distutils.command.sdist.sdist.finalize_options(self)
-        branch = ".".join(self.__version.split(".")[:2])
+        version = get_version()
+        sdist.finalize_options(self)
+        branch = ".".join(version.split(".")[:2])
         self.dist_dir = os.path.join(self.dist_dir, branch)
 
     def run(self):
         """Build tarballs and create additional files."""
+        version = get_version()
         if os.path.isfile("ChangeLog"):
             os.remove("ChangeLog")
-        os.system("tools/generate-change-log > ChangeLog")
+        run_command_or_exit("tools/generate-change-log > ChangeLog")
         assert os.path.isfile("ChangeLog")
         assert open("ChangeLog", "r").read().strip()
-        distutils.command.sdist.sdist.run(self)
-        basename = "nfoview-%s" % self.__version
+        sdist.run(self)
+        basename = "nfoview-%s" % version
         tarballs = os.listdir(self.dist_dir)
         os.chdir(self.dist_dir)
         # Compare tarball contents with working copy.
@@ -224,14 +233,14 @@ class SDistGna(distutils.command.sdist.sdist):
         for member in tobj.getmembers():
             tobj.extract(member, temp_dir)
         log.info("comparing tarball (tmp) with working copy (../..)")
-        os.system('diff -qr -x ".*" -x "*.pyc" ../.. %s' % test_dir)
+        run_command_or_exit('diff -qr -x ".*" -x "*.pyc" ../.. %s' % test_dir)
         response = raw_input("Are all files in the tarball [Y/n]? ")
         if response.lower() == "n":
             raise SystemExit("Must edit MANIFEST.in.")
         shutil.rmtree(test_dir)
         # Create extra distribution files.
         log.info("calculating md5sums")
-        os.system("md5sum * > %s.md5sum" % basename)
+        run_command_or_exit("md5sum * > %s.md5sum" % basename)
         log.info("creating '%s.changes'" % basename)
         source = os.path.join("..", "..", "ChangeLog")
         shutil.copyfile(source, "%s.changes" % basename)
@@ -240,12 +249,12 @@ class SDistGna(distutils.command.sdist.sdist):
         shutil.copyfile(source, "%s.news" % basename)
         for tarball in tarballs:
             log.info("signing '%s'" % tarball)
-            os.system("gpg --detach %s" % tarball)
+            run_command_or_exit("gpg --detach %s" % tarball)
 
 
 distutils.core.setup(
     name="nfoview",
-    version=get_version_number(),
+    version=get_version(),
     requires=("gtk",),
     platforms=("Platform Independent",),
     author="Osmo Salomaa",
