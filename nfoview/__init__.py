130523848a33e259d0f94c59cdb316a511fa3fef
Osmo Salomaa <otsaloma@iki.fi>
2013-04-08 21:38:24 +0300

    RELEASE 1.12

--- nfoview/__init__.py
+++ nfoview/__init__.py
@@ -27,7 +27,7 @@ Viewer for NFO files.
 :var conf: Instance of :class:`ConfigurationStore` used
 """
 
-__version__ = "1.11"
+__version__ = "1.12"
 
 from nfoview.paths import *
 from nfoview import deco
