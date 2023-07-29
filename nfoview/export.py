fb35bd46f72ce74d06987b49b49f5e36424b49d1
Osmo Salomaa <otsaloma@iki.fi>
2023-07-30 02:53:02 +0300

    Port export

--- nfoview/export.py
+++ nfoview/export.py
@@ -19,27 +19,14 @@ from gi.repository import GObject
 from gi.repository import Gtk
 from nfoview.i18n  import _
 
-try:
-    # Available since GTK 3.20.
-    Base = Gtk.FileChooserNative
-except AttributeError:
-    Base = Gtk.FileChooserDialog
 
-
-class ExportImageDialog(Base):
+class ExportImageDialog(Gtk.FileChooserNative):
 
     def __init__(self, parent):
         GObject.GObject.__init__(self)
+        self.set_accept_label(_("_Save"))
+        self.set_action(Gtk.FileChooserAction.SAVE)
+        self.set_cancel_label(_("_Cancel"))
+        self.set_select_multiple(False)
         self.set_title(_("Export Image"))
         self.set_transient_for(parent)
-        self.set_action(Gtk.FileChooserAction.SAVE)
-        if Base is Gtk.FileChooserNative:
-            self.set_cancel_label(_("_Cancel"))
-            self.set_accept_label(_("_Save"))
-        if Base is Gtk.FileChooserDialog:
-            self.add_button(_("_Cancel"), Gtk.ResponseType.CANCEL)
-            self.add_button(_("_Save"), Gtk.ResponseType.OK)
-            self.set_default_response(Gtk.ResponseType.OK)
-        # XXX:
-        # self.set_do_overwrite_confirmation(True)
-        self.set_select_multiple(False)
