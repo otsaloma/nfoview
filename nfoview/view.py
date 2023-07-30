3edd829b73901fe66d69d4906bf9fc228bf38df8
Osmo Salomaa <otsaloma@iki.fi>
2023-07-31 00:18:33 +0300

    Port view

--- nfoview/view.py
+++ nfoview/view.py
@@ -30,7 +30,22 @@ class TextView(Gtk.TextView):
         GObject.GObject.__init__(self)
         self._link_tags = []
         self._visited_link_tags = []
-        self._init_properties()
+        self.set_bottom_margin(6)
+        self.set_cursor_visible(False)
+        self.set_editable(False)
+        self.set_left_margin(6)
+        self.set_pixels_above_lines(nfoview.conf.pixels_above_lines)
+        self.set_pixels_below_lines(nfoview.conf.pixels_below_lines)
+        self.set_right_margin(6)
+        self.set_top_margin(6)
+        self.set_wrap_mode(Gtk.WrapMode.NONE)
+        controller = Gtk.EventControllerMotion()
+        controller.set_propagation_phase(Gtk.PropagationPhase.CAPTURE)
+        self.add_controller(controller)
+        controller.connect("motion", self._on_motion)
+        gesture = Gtk.GestureClick()
+        self.add_controller(gesture)
+        gesture.connect("pressed", self._on_pressed)
         self.update_style()
 
     def get_text(self):
@@ -38,98 +53,59 @@ class TextView(Gtk.TextView):
         start, end = text_buffer.get_bounds()
         return text_buffer.get_text(start, end, False)
 
-    def _init_properties(self):
-        pixels_above = nfoview.conf.pixels_above_lines
-        pixels_below = nfoview.conf.pixels_below_lines
-        self.set_cursor_visible(False)
-        self.set_editable(False)
-        self.set_wrap_mode(Gtk.WrapMode.NONE)
-        self.set_pixels_above_lines(pixels_above)
-        self.set_pixels_below_lines(pixels_below)
-        self.set_left_margin(6)
-        self.set_right_margin(6)
-        with nfoview.util.silent(AttributeError):
-            # Available since GTK 3.18.
-            self.set_top_margin(6)
-            self.set_bottom_margin(6)
-        # XXX:
-        # nfoview.util.connect(self, self, "motion-notify-event")
+    def _insert_text(self, text):
+        text_buffer = self.get_buffer()
+        itr = text_buffer.get_end_iter()
+        text_buffer.insert(itr, text)
 
     def _insert_url(self, url):
         text_buffer = self.get_buffer()
         tag = text_buffer.create_tag(None)
         tag.props.underline = Pango.Underline.SINGLE
-        # XXX:
-        # tag.connect("event", self._on_link_tag_event)
         tag.nfoview_url = url
         itr = text_buffer.get_end_iter()
         text_buffer.insert_with_tags(itr, url, tag)
         self._link_tags.append(tag)
 
-    def _insert_word(self, word):
-        text_buffer = self.get_buffer()
-        itr = text_buffer.get_end_iter()
-        text_buffer.insert(itr, word)
+    def _on_motion(self, controller, x, y, user_data=None):
+        window = Gtk.TextWindowType.WIDGET
+        x, y = self.window_to_buffer_coords(window, x, y)
+        if iter := self.get_iter_at_location(x, y):
+            for tag in iter.iter.get_tags():
+                if hasattr(tag, "nfoview_url"):
+                    return self.set_cursor(Gdk.Cursor.new_from_name("pointer"))
+        self.set_cursor(Gdk.Cursor.new_from_name("default"))
 
-    def _on_link_tag_event(self, tag, text_view, event, itr):
-        if event.type != Gdk.EventType.BUTTON_RELEASE: return
+    def _on_pressed(self, gesture, n_press, x, y, user_data=None):
         text_buffer = self.get_buffer()
         if text_buffer.get_selection_bounds(): return
-        nfoview.util.show_uri(tag.nfoview_url)
-        if tag in self._link_tags:
-            self._link_tags.remove(tag)
-            self._visited_link_tags.append(tag)
-            self.update_style()
-
-    def _on_motion_notify_event(self, text_view, event):
         window = Gtk.TextWindowType.WIDGET
-        x, y = self.window_to_buffer_coords(window, int(event.x), int(event.y))
-        window = self.get_window(Gtk.TextWindowType.TEXT)
-        tags = []
-        # Return value changed since GTK 3.20!?
-        iter = self.get_iter_at_location(x, y)
-        if isinstance(iter, tuple) and hasattr(iter, "iter"):
-            iter = iter.iter
-        with nfoview.util.silent(AttributeError):
-            tags = iter.get_tags()
-        if any(hasattr(x, "nfoview_url") for x in tags):
-            window.set_cursor(Gdk.Cursor.new_for_display(
-                Gdk.Display.get_default(), Gdk.CursorType.HAND2))
-            return True # to not call the default handler.
-        window.set_cursor(Gdk.Cursor.new_for_display(
-            Gdk.Display.get_default(), Gdk.CursorType.XTERM))
+        x, y = self.window_to_buffer_coords(window, x, y)
+        if iter := self.get_iter_at_location(x, y):
+            for tag in iter.iter.get_tags():
+                if hasattr(tag, "nfoview_url"):
+                    nfoview.util.show_uri(tag.nfoview_url)
+                    if tag in self._link_tags:
+                        self._link_tags.remove(tag)
+                        self._visited_link_tags.append(tag)
+                        self.update_style()
 
     def set_text(self, text):
-        re_url = re.compile(r"(\w+://(\S+\.)?\S+|www\.\S+)\.[\w\-.~:/?#\[\]@!$&'()*+,;=%]+")
-        self._link_tags = []
-        self._visited_link_tags = []
+        URL = r"(\w+://(\S+\.)?\S+|www\.\S+)\.[\w\-.~:/?#\[\]@!$&'()*+,;=%]+"
         text_buffer = self.get_buffer()
         bounds = text_buffer.get_bounds()
         text_buffer.delete(*bounds)
-        lines = text.split("\n")
-        # Scan text word-by-word for possible URLs, but insert words in
-        # larger chunks to avoid doing too many slow text view updates.
-        word_queue = []
-        for i, line in enumerate(lines):
-            words = line.split(" ")
-            for j, word in enumerate(words):
-                match = re_url.search(word)
-                if match is not None:
-                    a, z = match.span()
-                    word_queue.append(word[:a])
-                    self._insert_word("".join(word_queue))
-                    word_queue = []
-                    self._insert_url(word[a:z])
-                    word_queue.append(word[z:])
-                else: # Normal text.
-                    word_queue.append(word)
-                word_queue.append(" ")
-            word_queue.pop(-1)
-            word_queue.append("\n")
-            if len(word_queue) > 1000:
-                self._insert_word("".join(word_queue))
-                word_queue = []
-        self._insert_word("".join(word_queue))
+        self._link_tags = []
+        self._visited_link_tags = []
+        for line in text.splitlines():
+            i = 0
+            for match in re.finditer(URL, line):
+                a, z = match.span()
+                self._insert_text(line[i:a])
+                self._insert_url(line[a:z])
+                i = z
+            self._insert_text(line[i:])
+            self._insert_text("\n")
         self.update_style()
 
     def update_style(self):
